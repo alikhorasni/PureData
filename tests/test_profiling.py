68f605cc2e226f7a3536de08ddacb4fd6b6235d0
Ali <alikhorasanishirehjeyni@gmail.com>
2026-06-23 04:37:55 -0400

    Create test_profiling.py

---
+++ tests/test_profiling.py
@@ -0,0 +1,9 @@
+from pure_data.profiling import DynamicProfiler
+
+
+def test_drift_detection(large_dirty_df):
+    profiler = DynamicProfiler(large_dirty_df.lazy())
+    profile = profiler.generate_profile()
+    assert profile.column_profiles["value"].distribution_drift_score is not None
+    # Drift should be > 0 because we injected a mean shift
+    assert profile.column_profiles["value"].distribution_drift_score > 0.1
